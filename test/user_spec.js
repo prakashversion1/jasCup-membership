c3dc263cd8e486c4bedeebce214c211550ab0807
prakashversion1 <prakashjoshiversion1@gmail.com>
2015-05-05 08:51:51 +0545

    User module created

---
+++ test/user_spec.js
@@ -0,0 +1,35 @@
+var should = require("should");
+var User = require("../model/user");
+describe("User",function(){
+
+	describe("defaults",function(){
+		var user = {};
+
+		before(function(){
+			user = new User({email:"prakash@gmail.com"});		
+		});
+
+		it("email is prakash@gmail.com",function(){
+			user.email.should.equal("prakash@gmail.com");
+		});
+		it("has a authentication token",function(){
+			user.authenticationToken.should.be.defined;
+		});
+		it("has a pending status",function(){
+			user.status.should.equal("pending");
+		});
+		it("has a created date",function(){
+			user.createdAt.should.be.defined;
+		});
+		it("has a signInCount of 0",function(){
+			user.signInCount.should.equal(0);
+		});
+		it("has lastLoginAt",function(){
+			user.lastLoginAt.should.be.defined;
+		});
+		it("has a currentLoginAt",function(){
+			user.currentLoginAt.should.be.defined;
+		});
+	});
+
+}); 
